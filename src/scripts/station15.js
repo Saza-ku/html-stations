dfd3077cce96e44941322fea71b0f4b280f73b13
Saza-ku <the.latticeheart@gmail.com>
2022-01-02 10:39:00 +0000

    station15

--- src/scripts/station15.js
+++ src/scripts/station15.js
@@ -6,11 +6,24 @@ async function getData() {
     { id: 1, first_name: '優', family_name: '大木', affilication: 'TechTrain', is_student: false },
     { id: 2, first_name: '太郎', family_name: '山田', affilication: 'HogeHoge大学', is_student: true }
   ];
-  const result = await test()
-  return await result
+  await test()
+  return userList.map(buildFullName)
 }
 
 function test() {
-  return
+  return new Promise(resolve => setTimeout(resolve, 3000))
 }
 
+function buildFullName(data) {
+  // Station14の問題はこちらです。想定する答えをここを書いてください。
+  // 氏名がわかるようにしつつ、半角スペースで繋いでください。
+  // またtest配列のそれぞれのオブジェクトに対して、full_nameのプロパティが追加されるように実装すること
+  return {
+      id: data.id,
+      full_name: data.family_name + " " + data.first_name,
+      first_name: data.first_name,
+      family_name: data.family_name,
+      affilication: data.affilication,
+      is_student: data.is_student,
+  }
+}
